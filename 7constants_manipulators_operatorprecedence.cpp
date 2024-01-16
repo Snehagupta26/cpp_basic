d1323930fc372b1fa2e73b9c2bd8224ce894f362
Snehagupta26 <02snehagupta@gmail.com>
2024-01-16 22:55:50 +0530

    ok

---
+++ 7constants_manipulators_operatorprecedence.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a = 43;
+    cout<<"value of a was : "<<a<<endl;
+    a = 67;
+    cout<<"a value is : "<<a<<endl;
+    cout<<"conSTANT"<<endl;
+    const int b = 7;
+    cout<<b<<endl;
+    // b = 8;      
+    cout<<"MANIPULATORS : "<<endl;
+}
